e2e25b327dc89938088dd0d0235030b2353f44be
Joseph Greshik <Joseph_Greshik@comcast.com>
2019-07-09 12:27:14 -0600

    corrected input for noise function

--- Render.java
+++ Render.java
@@ -13,9 +13,11 @@ public class Render {
         Image image = new Image(w,h);
         for(int i=0;i<w;++i){
             for(int j=0;j<h;++j){
-                image.red[i][j]=(int)(SimplexNoise.noise(i,j)*255);
-                image.green[i][j]=(int)(SimplexNoise.noise(i,j)*255);
-                image.blue[i][j]=(int)(SimplexNoise.noise(i,j)*255);
+                double W=w;
+                double H=h;
+                image.red[i][j]=(int)(SimplexNoise.noise(i/W,j/H)*255);
+                image.green[i][j]=(int)(SimplexNoise.noise(i/W,j/H)*255);
+                image.blue[i][j]=(int)(SimplexNoise.noise(i/W,j/H)*255);
             }
         }
         image.savetofile("simplex.png");
