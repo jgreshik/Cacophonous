90a744513ae02b4737fe8c7412394cd669161798
Joseph Greshik <Joseph_Greshik@comcast.com>
2019-07-10 17:23:39 -0600

    save

--- Render.java
+++ Render.java
@@ -8,13 +8,13 @@ import java.util.*;
 
 public class Render {
     public static void main(String[]args)throws Throwable{
-        int w=4*1024;
+        int w=8*1024;
         int h=w;
         Image image = new Image(w,h);
         for(int i=0;i<w;++i){
             for(int j=0;j<h;++j){
-                double W=w;
-                double H=h;
+                double W=w/12;
+                double H=h/24;
                 image.red[i][j]=(int)(SimplexNoise.noise(i/W,j/H)*255);
                 image.green[i][j]=(int)(SimplexNoise.noise(i/W,j/H)*255);
                 image.blue[i][j]=(int)(SimplexNoise.noise(i/W,j/H)*255);
